ca9154dc7356c0122cb444352f17893726e3892a
Joner2008 <101406401+Joner2008@users.noreply.github.com>
2022-03-18 14:20:43 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
-function doSomething () {
-	
+function girar () {
+    if (input.isGesture(Gesture.TiltLeft)) {
+        basic.showArrow(ArrowNames.West)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        basic.showArrow(ArrowNames.East)
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        basic.showArrow(ArrowNames.South)
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        basic.showArrow(ArrowNames.North)
+    }
 }
 basic.forever(function () {
-	
+    if (input.isGesture(Gesture.TiltLeft)) {
+        girar()
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        girar()
+    }
+    if (input.isGesture(Gesture.LogoUp)) {
+        girar()
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        girar()
+    }
 })
